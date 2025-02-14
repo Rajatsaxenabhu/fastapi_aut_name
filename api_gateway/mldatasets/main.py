b314e4a07f4d3e0b43af70fc4cdf90909845ccb3
Your Name <you@example.com>
2025-02-14 10:48:46 +0530

    done_okay

--- api_gateway/mldatasets/main.py
+++ api_gateway/mldatasets/main.py
@@ -14,7 +14,6 @@ class FileUploadRequest(BaseModel):
     file_name: str
     files: List[dict]
 
-
 def re_encode(content: str) -> str:  
     encoded= base64.b64decode(content).decode('utf-8')
     return encoded
@@ -28,8 +27,12 @@ def re_encode_img(content: str) -> str:
 @app.post('/form_files',status_code=status.HTTP_201_CREATED)
 async def image_upload_multiple(data:FileUploadRequest):
     try:
-        print("iniside  the data")
-        print("data",data.file_name)
+        for i in data.files:
+            if i['content_type'].split('/')[0] == 'image':
+                print("image file actins started")
+            elif i['content_type'].split('/')[0] == 'text':
+                print("text file actions started")
+                print(re_encode(i['content']))
         return JSONResponse(content={"message":"formdata successful"},status_code=status.HTTP_201_CREATED)
     except Exception as err:
         print("excepr from ml dataet ",str(err))
